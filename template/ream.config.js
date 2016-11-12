7876bcea37d77adb8d19f6f9a7a2336fae4c2e9a
EGOIST <0x142857@gmail.com>
2016-11-13 02:26:28 +0800

    add ream config

---
+++ template/ream.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // externals modules for server bundle
+  externals: ['vue', 'vuex', 'vue-router'],
+  // common chunk for client bundle
+  common: ['vue', 'vuex', 'vue-router']
+}
